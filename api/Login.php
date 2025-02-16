f16de52f169ffff4029f32db8a992248807cd234
DimChig <dima.chigarev@gmail.com>
2025-02-15 21:10:28 -0500

    Replaced email to login

--- api/Login.php
+++ api/Login.php
@@ -26,23 +26,23 @@ function returnWithError($err) {
 
 // return info as json
 function returnWithInfo($id, $login) {
-    sendResultInfoAsJson(["success" => true, "id" => $id, "email" => $login]);
+    sendResultInfoAsJson(["success" => true, "id" => $id, "login" => $login]);
 }
 
 // store user input data
 $inData = getRequestInfo();
 
 // define values from user input
-$email = $inData["email"] ?? null;
+$login = $inData["login"] ?? null;
 $password = $inData["password"] ?? null;
 
 // if either fields are missing, return error
-if (!$email || !$password) {
-    returnWithError("Missing required fields: email and password");
+if (!$login || !$password) {
+    returnWithError("Missing required fields: login and password");
 }
 
-// establish a connection with the database
-$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "smallProjectTest");
+// load db connection
+require_once __DIR__ . "/db_connector.php";
 
 // check if connection is successful or not
 if ($conn->connect_error) {
@@ -51,23 +51,23 @@ if ($conn->connect_error) {
 
 // if connection is successful, find user in the database
 $stmt = $conn->prepare("SELECT ID, Password FROM Users WHERE Login=?");
-$stmt->bind_param("s", $email);
+$stmt->bind_param("s", $login);
 $stmt->execute();
 $result = $stmt->get_result();
 
 // check if the returned password matches the one the user sent in
 if ($row = $result->fetch_assoc()) {
         if ($password === $row["Password"]) {
-                returnWithInfo($row["ID"], $email);
+                returnWithInfo($row["ID"], $login);
         }
         // otherwise, tell the user they used the incorrect password
         else {
                 returnWithError("Incorrect password");
         }
 }
-// otherwise, tell the user their email is not associated with an account
+// otherwise, tell the user their login is not associated with an account
 else {
-        returnWithError("No records found for this email");
+        returnWithError("No records found for this login");
 }
 
 // close connections
